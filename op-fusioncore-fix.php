47951cc76f0ab486a357e06a64fc7eff19abc0d6
Davor Jambor <davor.jambor@gmail.com>
2016-08-01 14:50:33 +0200

    Added fix for Fusion Core version 2.0

--- op-fusioncore-fix.php
+++ op-fusioncore-fix.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              http://www.optimizepress.com/
- * @since             1.0.0
+ * @since             1.0.2
  * @package           Op_FusionCore_Fix
  *
  * @wordpress-plugin
  * Plugin Name:       OptimizePress Fusion Core fix
  * Plugin URI:        http://www.optimizepress.com/
- * Description:       Removes "image" shortcode from Fusion Core plugin on Live Editor pages
- * Version:           1.0.1
+ * Description:       Fix problem with RTE in LiveEditor and forces images/testimonial shortcode from OptimizePress
+ * Version:           1.0.2
  * Author:            OptimizePress
  * Author URI:        http://www.optimizepress.com/
  * License:           GPL-2.0+
@@ -31,21 +31,48 @@ if ( ! defined( 'WPINC' ) ) {
 }
 add_action('init' , 'op_fusioncore_shortcodes', 99);
 
-function op_fusioncore_shortcodes(){
-    $checkIfLEPage = get_post_meta( url_to_postid( "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] ), '_optimizepress_pagebuilder', true );
+add_action('init', 'op_fusioncore_shortcodeJSFix', 999999999999);
 
-    $pageBuilder = false;
-    if ( isset($_GET['page']) ) {
-        $pageBuilder = ($_GET['page'] == 'optimizepress-page-builder' ) ? true : false;
-    }
-    $liveEditorAjaxInsert = false;
-    if ( isset($_REQUEST['action']) ) {
-        $liveEditorAjaxInsert = ($_REQUEST['action'] == 'optimizepress-live-editor-parse' ) ? true : false;
-    }
+if (!function_exists('op_fusioncore_shortcodes')){
+    function op_fusioncore_shortcodes(){
+        $checkIfLEPage = get_post_meta( url_to_postid( "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] ), '_optimizepress_pagebuilder', true );
 
-    if ( ($checkIfLEPage == 'Y') || $pageBuilder || $liveEditorAjaxInsert ){
-        global $shortcode_tags;
-        $shortcode_tags['images'][0] = 'OptimizePress_Default_Assets';
-        $shortcode_tags['images'][1] = 'images';
+        $pageBuilder = false;
+        if ( isset($_GET['page']) ) {
+            $pageBuilder = ($_GET['page'] == 'optimizepress-page-builder' ) ? true : false;
+        }
+        $liveEditorAjaxInsert = false;
+        if ( isset($_REQUEST['action']) ) {
+            $liveEditorAjaxInsert = ($_REQUEST['action'] == 'optimizepress-live-editor-parse' ) ? true : false;
+        }
+
+        if ( ($checkIfLEPage == 'Y') || $pageBuilder || $liveEditorAjaxInsert ){
+            global $shortcode_tags;
+            $shortcode_tags['images'][0] = 'OptimizePress_Default_Assets';
+            $shortcode_tags['images'][1] = 'images';
+
+            $shortcode_tags['testimonials'][0] = 'OptimizePress_Default_Assets';
+            $shortcode_tags['testimonials'][1] = 'testimonials';
+        }
+    }
+}
+
+if (!function_exists('op_fusioncore_shortcodeJSFix')) {
+    function op_fusioncore_shortcodeJSFix()
+    {
+        $checkIfLEPage = get_post_meta(url_to_postid("http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']), '_optimizepress_pagebuilder', true);
+
+        $pageBuilder = false;
+        if (isset($_GET['page'])) {
+            $pageBuilder = ($_GET['page'] == 'optimizepress-page-builder') ? true : false;
+        }
+        $liveEditorAjaxInsert = false;
+        if (isset($_REQUEST['action'])) {
+            $liveEditorAjaxInsert = ($_REQUEST['action'] == 'optimizepress-live-editor-parse') ? true : false;
+        }
+
+        if ($pageBuilder || $liveEditorAjaxInsert) {
+            remove_filter('mce_external_plugins', array(FusionCore_Plugin::get_instance(), 'add_rich_plugins'), 10);
+        }
     }
 }
